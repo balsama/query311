761b0e1fac7dbb124ffea708e726d22067e7ebe7
Adam Balsam <balsam.adam@gmail.com>
2023-11-28 01:36:44 -0500

    Add nav to search results

--- public/starter/submit.php
+++ public/starter/submit.php
@@ -37,6 +37,24 @@ EOD;
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<div class="container">
+    <a class="navbar-brand" href="index.php">Boston 311 Record Search</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.php">Search by Zip Code and Case title</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="by-address.php">Search by address</a>
+            </li>
+        </ul>
+    </div>
+</div>
+</nav>
 <div class="container">
     <h1>Results:</h1>
     <p>
