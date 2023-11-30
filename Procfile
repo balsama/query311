d6d6812c8f1e1a789ae4a77c5fcf2fbd730d6851
Adam Balsam <balsam.adam@gmail.com>
2023-11-29 23:57:26 -0500

    Single form

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-apache2 public/starter/
+web: vendor/bin/heroku-php-apache2 public/
--- public/index.php
+++ public/index.php
@@ -5,25 +5,39 @@ use Balsama\Query311\Helpers;
 <!doctype html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Search Boston 311</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Search Boston 311</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+      <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+      <link rel="manifest" href="site.webmanifest">
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@balsama" />
+      <meta name="twitter:creator" content="@balsama" />
+      <meta property="og:url" content="http://localhost/query311/public/" />
+      <meta property="og:title" content="Boston 311 Records Search" />
+      <meta property="og:description" content="A tool to help you to search Boston's 311 records." />
+      <meta property="og:image" content="favicon/311Query.png" />
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
+        <a class="navbar-brand" href="index.php">
+          <img src="favicon/logo-light.png" alt="Bootstrap" width="40" height="40">
+        </a>
         <a class="navbar-brand" href="index.php">Boston 311 Record Search</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Search by Zip Code and Case title</a>
+              <a class="nav-link active" aria-current="page" href="index.php">Search</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="by-address.php">Search by address</a>
+              <a class="nav-link" href="about.php">About</a>
             </li>
           </ul>
         </div>
@@ -37,14 +51,14 @@ use Balsama\Query311\Helpers;
         <form method="get" enctype="multipart/form-data" action="submit.php" name="type">
 
             <div class="form-floating">
-            <select class="form-select form-select-lg mb-3" name="table" id="yearselect" aria-label="Default select example" onchange="updateDateFields(event)">
-                <?php
-                foreach (Helpers::YEAR_UUID_MAP as $year => $uuid) {
-                    echo "<option value='$year'>$year</option>";
-                }
-                ?>
-            </select>
-                <label class="form-label" for="table">Year to search</label>
+                <select class="form-select form-select-lg mb-3" name="table" id="yearselect" aria-label="Default select example" onchange="updateDateFields(event)">
+                    <?php
+                    foreach (Helpers::YEAR_UUID_MAP as $year => $uuid) {
+                        echo "<option value='$year'>$year</option>";
+                    }
+                    ?>
+                </select>
+                <label class="form-label" for="yearselect">Year to search</label>
             </div>
 
             <ul class="nav nav-tabs" id="myTab" role="tablist">
@@ -57,14 +71,86 @@ use Balsama\Query311\Helpers;
             </ul>
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="tab_zip" role="tabpanel" aria-labelledby="home-tab">
-                    <div class="form-floating">
+                    <div class="input-group mb-3">
+                       <div class="form-floating">
                         <input type="text" class="form-control" placeholder="Zip code" aria-label="zip-code" name="zip-code" id="zip-code">
                         <label class="form-label" for="case-title-contains">Zip code</label>
                     </div>
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control mt-3" placeholder="Case title contains" aria-label="case-title-contains" name="case-title-contains" id="case-title-contains">
-                        <label class="form-label" for="case-title-contains">Case title contains (optional)</label>
-                        <small class="text-muted mb-3">Case sensitive. Try <code>Trash</code>, <code>Rodent</code>, or <code>Unsatisfactory</code></small>
+                       <div class="form-floating">
+                        <select class="form-select" aria-label="case-title-contains" name="case-title-contains" id="case-title-contains">
+                            <option selected="selected" value="">- Any -</option>
+                            <option value="Bicycle">Abandoned Bicycle</option>
+                            <option value="Building">Abandoned Building</option>
+                            <option value="Vehicle">Abandoned Vehicle</option>
+                            <option value="Bed Bugs">Bed Bugs</option>
+                            <option value="Broken Park Equipment">Broken Park Equipment</option>
+                            <option value="Broken Sidewalk">Broken Sidewalk</option>
+                            <option value="Building Inspection Request">Building Inspection Request</option>
+                            <option value="BWSC">BWSC</option>
+                            <option value="atchbasin">Catchbasin</option>
+                            <option value="E Collection">Code Enforcement Collection</option>
+                            <option value="emetery Maintenance Request">Cemetery Maintenance Request</option>
+                            <option value="omplaint">Complaint/Contractor Complaint</option>
+                            <option value="onstruction Debris">Construction Debris</option>
+                            <option value="Damaged Sign">Damaged Sign</option>
+                            <option value="Dead Tree Removal">Dead Tree Removal</option>
+                            <option value="Exceeding Terms of Permit">Exceeding Terms of Permit</option>
+                            <option value="Fire">Fire Department Request</option>
+                            <option value="General">General</option>
+                            <option value="Graffiti">Graffiti</option>
+                            <option value="Ground Maintenance">Ground Maintenance</option>
+                            <option value="Heat">Heat (Excessive, Insufficient)</option>
+                            <option value="Hero Square Sign Request">Hero Square Sign Request</option>
+                            <option value="HP Sign">HP Sign</option>
+                            <option value="Illegal Dumping">Illegal Dumping</option>
+                            <option value="Illegal Graffiti">Illegal Graffiti</option>
+                            <option value="Parking">Illegal Parking</option>
+                            <option value="Improper Storage of Trash (Barrels)">Improper Storage of Trash (Barrels)</option>
+                            <option value="Litter">Litter</option>
+                            <option value="Mice">Mice Infestation (Residential)</option>
+                            <option value="Missed">Missed Trash or Recycling</option>
+                            <option value="Missing Sign">Missing Sign</option>
+                            <option value="Needle">Needle Clean-up</option>
+                            <option value="New Sign">New Sign, Crosswalk or Marking</option>
+                            <option value="New Tree">New Tree Requests</option>
+                            <option value="Notification">Notification</option>
+                            <option value="Other">Other</option>
+                            <option value="Park ">Park Maintenance</option>
+                            <option value="Pigeon">Pigeon Infestation</option>
+                            <option value="Poor Conditions of Property">Poor Conditions of Property</option>
+                            <option value="Pothole">Pothole</option>
+                            <option value="Protection of Adjoining Property">Protection of Adjoining Property</option>
+                            <option value="Public Events">Public Events Noise Disturbances</option>
+                            <option value="Public Works">Public Works General Request</option>
+                            <option value="Rat">Rat</option>
+                            <option value="Recycling Cart">Recycling Cart</option>
+                            <option value="Plowing">Roadway Plowing/Salting</option>
+                            <option value="Rodent">Rodent Sighting</option>
+                            <option value="Bulk Item">Schedule a Bulk Item Pickup</option>
+                            <option value="Senior Shoveling">Senior Shoveling</option>
+                            <option value="Sewage">Sewage/Septic Back-Up</option>
+                            <option value="Short Term Rental">Short Term Rental</option>
+                            <option value="Sidewalk">Sidewalk</option>
+                            <option value="Snow">Snow</option>
+                            <option value="Space Savers">Space Savers</option>
+                            <option value="Squalid">Squalid Living Conditions</option>
+                            <option value="Street Light">Street Lights</option>
+                            <option value="Move-In">Student Move-In Issues</option>
+                            <option value="Traffic Signal">Traffic Signal</option>
+                            <option value="Trash">Trash</option>
+                            <option value="Tree Emergencies">Tree Emergencies</option>
+                            <option value="Unsafe">Unsafe Dangerous Conditions</option>
+                            <option value="Unsanitary">Unsanitary Conditions</option>
+                            <option value="Unsatisfactory">Unsatisfactory Living Conditions</option>
+                            <option value="Valet">Valet Parking Problems</option>
+                            <option value="Work Hours">Work Hours-Loud Noise Complaints</option>
+                            <option value="Work w/out Permit">Work w/out Permit</option>
+                            <option value="Working Beyond Hours">Working Beyond Hours</option>
+                            <option value="Zoning">Zoning</option>
+                        </select>
+                        </select>
+                        <label class="form-label" for="case-title-contains">Category (optional)</label>
+                    </div>
                     </div>
                 </div>
                 <div class="tab-pane fade" id="tab_address" role="tabpanel" aria-labelledby="profile-tab">
@@ -73,7 +159,7 @@ use Balsama\Query311\Helpers;
                         <label class="form-label" for="address-contains">Address (starts with)</label>
                         <div class="form-check form-switch">
                             <input class="form-check-input" type="checkbox" role="switch" id="address-search-type" name="address-search-type">
-                            <label class="form-check-label" for="count">Address "contains" (default: "starts with")</label>
+                            <label class="form-check-label" for="address-search-type">Address "contains" (default: "starts with")</label>
                         </div>
                         <small class="text-muted">Case sensitive. Try <code>36 Hull</code> or <code>55 Boutwell</code>, for example</small>
                     </div>
