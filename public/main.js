f542dfa25d90b7a1f11b0550932666d1f0255cb1
Adam Balsam <balsam.adam@gmail.com>
2023-12-05 20:29:05 -0500

    Fixed #5: Update label for address field when search type is toggled

--- public/main.js
+++ public/main.js
@@ -1,5 +1,13 @@
+function updateAddressLabel(event){
+    if (event.currentTarget.checked) {
+        document.getElementById("address-contains-label").innerText = "Address (contains)";
+    }
+    else {
+        document.getElementById("address-contains-label").innerText = "Address (starts with)";
+    }
+}
 function updateDateFields(event){
-    const selectedValue = event.target.value
+    const selectedValue = event.target.value;
     document.getElementById("date-after").value = selectedValue + "-01-01";
     document.getElementById("date-before").value = selectedValue + "-12-31";
 }
